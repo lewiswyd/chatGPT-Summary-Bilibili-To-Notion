eafe15e5dd40e78bacb80cd54960a0d5e6924874
lewiswyd <scaulewis@gmail.com>
2024-10-08 18:24:45 +0800

    Update bili_video_info_downloader.py

--- bili_video_info_downloader/bili_video_info_downloader.py
+++ bili_video_info_downloader/bili_video_info_downloader.py
@@ -3,17 +3,26 @@ from bili_video_info_downloader.section_info import section_dict
 
 
 class VideoInfoDownloader:
-    def __init__(self, bv_id: str) -> None:
+    def __init__(self, bv_id: str, cookie: str) -> None:
         self.bv_id = bv_id
         self.info_api = "https://api.bilibili.com/x/web-interface/view"
         self.tags_api = "https://api.bilibili.com/x/web-interface/view/detail/tag"
+        self.headers = {
+            'authority': 'api.bilibili.com',
+            'accept': 'application/json, text/plain, */*',
+            'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
+            'origin': 'https://www.bilibili.com',
+            'referer': 'https://www.bilibili.com/',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
+            'Cookie': cookie
+        }
 
 
     def _get_info(self):
         params = (
             ('bvid', self.bv_id),
         )
-        response = requests.get(self.info_api, params=params)
+        response = requests.get(self.info_api, headers=self.headers, params=params)
         return response.json()['data']
 
 
@@ -22,7 +31,7 @@ class VideoInfoDownloader:
             ('bvid', self.bv_id),
         )
 
-        response = requests.get(self.tags_api, params=params)
+        response = requests.get(self.tags_api, headers=self.headers, params=params)
         data = response.json()['data']
         if data:
             tags = [x['tag_name'] for x in data]
